3d9a7e043b6438e910e5804ac9f2c49323201f21
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-01-18 14:18:52 +0100

    adds inventory and inventoryData

---
+++ Assets/Scripts/Runes/Inventory/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runes
+{
+    public class Inventory : MonoBehaviour
+    {
+        public InventoryData data;
+    }
+}
