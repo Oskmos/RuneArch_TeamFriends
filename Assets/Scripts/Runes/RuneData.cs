ccaa352851c98d8381f1fb5ea66c2a05c3450452
Oskar Jansson <oskar_slim89@hotmail.com>
2021-01-18 11:57:42 +0100

    Adds RuneData script

---
+++ Assets/Scripts/Runes/RuneData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runes
+{
+    public class RuneData
+    {
+
+        public RuneRarity RuneRarity;
+
+        public RuneType RuneType;
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+    public class RuneRarity : ScriptableObject
+    {
+    
+    }
+
+    public class RuneType : ScriptableObject
+    {
+        
+    }
+}
