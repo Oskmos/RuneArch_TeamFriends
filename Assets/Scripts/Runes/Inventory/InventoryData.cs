90a7a6b5a708b70d2b1ba593726088ddd938dfb8
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2021-01-18 14:33:40 +0100

    adds runedata amount variable

--- Assets/Scripts/Runes/Inventory/InventoryData.cs
+++ Assets/Scripts/Runes/Inventory/InventoryData.cs
@@ -8,5 +8,31 @@ namespace Runes
     public class InventoryData
     {
         public List<RuneData> runes = new List<RuneData>();
+
+        public void AddRune(RuneData runeData)
+        {
+            RuneData rune = GetRune(runeData);
+            if(rune == null)
+            {
+                //runes.Add(new RuneData(rune));
+            }
+        }
+
+        public bool ContainsRune(RuneData runeData)
+        {
+            return GetRune(runeData) != null;
+        }
+
+        public RuneData GetRune(RuneData runeData)
+        {
+            foreach (var rune in runes)
+            {
+                if (rune.runeRarity == runeData.runeRarity && rune.runeType == runeData.runeType)
+                {
+                    return rune;
+                }
+            }
+            return null;
+        }
     }
 }
--- Assets/Scripts/Runes/RuneData.cs
+++ Assets/Scripts/Runes/RuneData.cs
@@ -5,5 +5,18 @@ namespace Runes {
     public class RuneData {
         public RuneType runeType;
         public RuneRarity runeRarity;
+        public int amount;
+
+        public RuneData(RuneData runeData)
+        {
+            runeType = runeData.runeType;
+            runeRarity = runeData.runeRarity;
+        }
+
+        public void CombineWith(RuneData runeData)
+        {
+            amount += runeData.amount;
+            runeData.amount -= amount;
+        }
     }
 }
