e5a92af3352219a44850b0e518e3234f92aff1c4
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-01-19 14:16:34 +0100

    Add drag and drop

---
+++ Assets/Scripts/Runes/DropSlot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Runes{
+    public class DropSlot : MonoBehaviour, IDropHandler, IPointerClickHandler
+    {
+        //TODO: Polish feature remove other rarities when adding a new one
+        
+        public void OnDrop(PointerEventData eventData){
+            if (eventData.pointerDrag == null) return;
+            RemoveOldRune();
+            //TODO: Check if empty
+            eventData.pointerDrag.GetComponent<SelectedRune>().rectTransform.SetParent(transform);
+            eventData.pointerDrag.GetComponent<SelectedRune>().rectTransform.anchoredPosition = Vector2.zero;
+            //TODO:Send data to merge inventory
+        }
+
+        public void OnPointerClick(PointerEventData eventData){
+            RemoveOldRune();
+        }
+
+        void RemoveOldRune(){
+            if(transform.childCount == 0)
+                return;
+            Destroy(transform.GetChild(0).gameObject);
+            
+            //TODO: Send data to inventory and merge inventory
+        }
+    }
+}
--- Assets/Scripts/Runes/SelectedRune.cs
+++ Assets/Scripts/Runes/SelectedRune.cs
@@ -6,16 +6,35 @@ using UnityEngine.EventSystems;
 namespace Runes {
     [Serializable]
     public class RuneEvent : UnityEvent<RuneData> {}
-    public class SelectedRune : MonoBehaviour, IPointerClickHandler {
-        [SerializeField] RuneEvent runeEvent;
-        RuneData _data;
-        
-        void Awake() {
-             _data = GetComponent<RuneData>();
+    public class SelectedRune : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+        [SerializeField]RuneEvent runeEvent;
+        [SerializeField]Canvas canvas;
+        [SerializeField]Rune runePrefab;
+        RuneData runeData;
+        CanvasGroup canvasGroup;
+        [HideInInspector]public RectTransform rectTransform;
+
+        void Awake(){
+            runeData = GetComponent<Rune>().RuneData;
+            
+        }
+        public void OnPointerDown(PointerEventData eventData){
+        }
+        public void OnBeginDrag(PointerEventData eventData){
+            var instance = Instantiate(runePrefab, transform.position, Quaternion.identity, canvas.transform);
+            instance.RuneData = runeData;
+            canvasGroup = instance.GetComponent<CanvasGroup>();
+            rectTransform = instance.GetComponent<RectTransform>();
+            canvasGroup.blocksRaycasts = false;
+            //Remove one count and the new one only have one.
+        }
+        public void OnEndDrag(PointerEventData eventData){
+            //runeEvent?.Invoke(_data);
+            
         }
 
-        public void OnPointerClick(PointerEventData eventData) {
-            runeEvent?.Invoke(_data);
+        public void OnDrag(PointerEventData eventData){
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
         }
     }
 }
