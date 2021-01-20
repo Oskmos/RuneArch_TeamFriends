2858a89b71acf73934fbbd135fd9ecc0e48fbd1b
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-01-20 16:46:40 +0100

    Adds click to move rune

--- Assets/Scripts/Runes/MoveToTargetInventory.cs
+++ Assets/Scripts/Runes/MoveToTargetInventory.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Runes{
     
-    public class MoveToTargetInventory : MonoBehaviour{
+    public class MoveToTargetInventory : MonoBehaviour, IPointerClickHandler{
         [SerializeField]InventoryData moveToInventoryData;
         InventoryData inventoryData;
         List<RuneData> runeDataList = new List<RuneData>();
-
         void Start(){
             inventoryData = GetComponent<InventoryData>();
         }
@@ -28,6 +28,16 @@ namespace Runes{
                 inventoryData.RemoveRune(runeData);
             }
             runeDataList.Clear();
+        }
+
+        public void OnPointerClick(PointerEventData eventData){
+            var selectedRune = eventData.hovered.Find(x => x.GetComponent<Rune>());
+            if(selectedRune == null)
+                return;
+            var runeData = new RuneData(selectedRune.GetComponent<Rune>().RuneData);
+            runeData.amount = 1;
+            moveToInventoryData.AddRune(runeData);
+            inventoryData.RemoveRune(runeData);
         }
     }
 }
